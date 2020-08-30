76143d6d1b8b47548de5c786f4388243bf88171f
Nakul Jain <nakuljain20@gmail.com>
2020-08-31 00:02:36 +0530

    first commit

---
+++ passport-mongo/app.js
@@ -0,0 +1,48 @@
+var express = require('express');
+// var path = require('path');
+var port     = process.env.PORT || 8080;
+var morgan = require('morgan');
+var dotenv = require('dotenv');
+dotenv.config();
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var flash    = require('connect-flash');
+var session = require('express-session');
+
+var routes = require('./routes/routes');
+// mongoose.connect(dbConfig.url);
+var uri = process.env.MongoDB_URL;
+mongoose.connect(uri, { 
+     useNewUrlParser: true,  
+     useUnifiedTopology: true
+    })
+     .then(() => { 
+          console.log("MongoDB Connected…")
+        })
+    .catch(err => console.log(err))
+
+var app = express();
+
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+app.use(session({ secret: 'SECRET' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+;
+// app.use(express.static(path.join(__dirname, 'public')));
+
+require('./config/passport')(passport);
+require('./routes/routes.js')(app, passport);
+app.use('/', routes);
+app.listen(port);
+
+
+
+module.exports = app;
---
+++ passport-mongo/routes/routes.js
@@ -0,0 +1,70 @@
+module.exports = function(app, passport) {
+
+  app.get('/', function(req, res) {
+      res.render('index.ejs'); 
+  });
+
+  app.get('/login', function(req, res) {
+
+      res.render('login.ejs', { message: req.flash('loginMessage') }); 
+  });
+
+ 
+  app.get('/signup', function(req, res) {
+
+      res.render('signup.ejs', { message: req.flash('signupMessage') });
+  });
+
+  app.get('/reset', function(req, res) {
+
+    res.render('reset.ejs', { message: req.flash('resetMessage') });
+  });
+
+
+  app.get('/profile', isLoggedIn, function(req, res) {
+      res.render('profile.ejs', {
+          user : req.user 
+      });
+  });
+
+
+
+  app.get('/logout', function(req, res) {
+      req.logout();
+      res.redirect('/');
+  });
+
+  app.post('/signup', passport.authenticate('local-signup', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/signup', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  app.post('/login', passport.authenticate('local-login', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/login', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+
+  app.post('/reset', passport.authenticate('password-reset', {
+    successRedirect : '/profile', // redirect to the secure profile section
+    failureRedirect : '/reset', // redirect back to the reset page if there is an error
+    failureFlash : true // allow flash messages
+  }));
+  app.get('/auth/google', passport.authenticate('google', { scope : ['profile', 'email'] }));
+
+  // the callback after google has authenticated the user
+  app.get('/auth/google/callback',
+          passport.authenticate('google', {
+                  successRedirect : '/profile',
+                  failureRedirect : '/'
+          }));
+}
+
+function isLoggedIn(req, res, next) {
+ 
+  if (req.isAuthenticated())
+      return next();
+
+  res.redirect('/');
+}
